af3ff9851003d0a77a6f284e595d82266819aa80
jonag <jonag@jonag.org>
2016-10-31 18:35:36 +0100

    Adding a phpunit test
    Updating to symfony/console 3.2.0 to be able to fix the terminal size for the tests

--- src/jonag/Episodes/Tests/Command/MoveEpisodesCommandTest.php
+++ src/jonag/Episodes/Tests/Command/MoveEpisodesCommandTest.php
@@ -24,30 +24,37 @@ class MoveEpisodesCommandTest extends \PHPUnit_Framework_TestCase
         ]);
     }
 
-//    public function testIgnoreSamples()
-//    {
-//        $container = $this->application->getContainer();
-//
-//        $container['config'] = [
-//            'source_directory' => vfsStream::url('Episodes/From'),
-//            'target_directory' => vfsStream::url('Episodes/To'),
-//            'ignore_if_nuked' => false,
-//            'delete_nuked' => false,
-//            'search_subtitles' => false,
-//            'prefer_move_over_copy' => false
-//        ];
-//
-//        copy(
-//            __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Fixtures/breakdance.mp4',
-//            vfsStream::url('Episodes/From/sample-angie.tribeca.s01e07.720p.hdtv.x264-killers.mkv')
-//        );
-//
-//        $commandTester = new CommandTester($this->application->find('episodes:move'));
-//        $commandTester->execute([]);
-//
-//        $this->assertContains('File sample-angie.tribeca.s01e07.720p.hdtv.x264-killers.mkv ignored because it\'s a sample', $commandTester->getDisplay());
-//        $this->assertEquals([], vfsStream::inspect(new vfsStreamStructureVisitor())->getStructure()['Episodes']['To'], 'Target directory is empty');
-//    }
+    public function testIgnoreSamples()
+    {
+        $container = $this->application->getContainer();
+
+        $container['config'] = [
+            'source_directory' => vfsStream::url('Episodes/From'),
+            'target_directory' => vfsStream::url('Episodes/To'),
+            'ignore_if_nuked' => false,
+            'delete_nuked' => false,
+            'search_subtitles' => false,
+            'prefer_move_over_copy' => false
+        ];
+
+        copy(
+            __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Fixtures/breakdance.mp4',
+            vfsStream::url('Episodes/From/sample-angie.tribeca.s01e07.720p.hdtv.x264-killers.mkv')
+        );
+
+        putenv('COLUMNS=80');
+        $commandTester = new CommandTester($this->application->find('episodes:move'));
+        $commandTester->execute([]);
+
+        $expected = <<<'EXPECTED'
+ ! [NOTE] File sample-angie.tribeca.s01e07.720p.hdtv.x264-killers.mkv ignored   
+ !        because it's a sample  
+EXPECTED;
+
+
+        $this->assertContains($expected, $commandTester->getDisplay());
+        $this->assertEquals([], vfsStream::inspect(new vfsStreamStructureVisitor())->getStructure()['Episodes']['To'], 'Target directory is empty');
+    }
 
     public function testNormalizeShowName()
     {
